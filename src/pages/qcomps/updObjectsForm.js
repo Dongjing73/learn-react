fb5d3ba4ea54f2a471fd7ec01956ba742bd9a15d
Dongjing Li <dongjingli@LMacBook-Pro.local>
2024-02-23 21:48:58 -0500

    in class

--- src/pages/qcomps/updObjectsForm.js
+++ src/pages/qcomps/updObjectsForm.js
@@ -1,60 +1,60 @@
 import { useState } from 'react';
 
 export default function Scoreboard() {
-  const [player, setPlayer] = useState({
-                                         firstName: 'John Woodrow',
-                                         lastName: 'Wilson',
-                                         likescore: 10,
-                                       });
+    const [player, setPlayer] = useState({
+                                             firstName: 'John Woodrow',
+                                             lastName: 'Wilson',
+                                             likescore: 10,
+                                         });
 
-  function handlePlusClick() {
-    // player.likescore++;
-    setPlayer({
-                ...player,
-                score: player.score + 1
-              });
-  }
+    function handlePlusClick() {
+        // player.likescore++;
+        setPlayer({
+                      ...player,
+                      score: player.score + 1
+                  });
+    }
 
-  function handleFirstNameChange(e) {
-    setPlayer({
-                ...player,
-                firstName: e.target.value,
-              });
-  }
+    function handleFirstNameChange(e) {
+        setPlayer({
+                      ...player,
+                      firstName: e.target.value,
+                  });
+    }
 
-  function handleLastNameChange(e) {
-    // setPlayer({
-    //   lastName: e.target.value
-    // });
-    setPlayer({
-                ...player,
-                lastName: e.target.value
-              });
-  }
+    function handleLastNameChange(e) {
+        // setPlayer({
+        //   lastName: e.target.value
+        // });
+        setPlayer({
+                      ...player,
+                      lastName: e.target.value
+                  });
+    }
 
-  return (
-      <>
-        <label>
-          Like Score: <b>{player.likescore}</b>
-          {'  '}
-          <button onClick={handlePlusClick}>
-            +1
-          </button>
-        </label>
-        <label>
-          First name:
-          <input
-              value={player.firstName}
-              onChange={handleFirstNameChange}
-          />
-        </label>
-        <label>
-          Last name:
-          <input
-              value={player.lastName}
-              onChange={handleLastNameChange}
-          />
-        </label>
-      </>
-  );
+    return (
+        <>
+            <label>
+                Like Score: <b>{player.likescore}</b>
+                {'  '}
+                <button onClick={handlePlusClick}>
+                    +1
+                </button>
+            </label>
+            <label>
+                First name:
+                <input
+                    value={player.firstName}
+                    onChange={handleFirstNameChange}
+                />
+            </label>
+            <label>
+                Last name:
+                <input
+                    value={player.lastName}
+                    onChange={handleLastNameChange}
+                />
+            </label>
+        </>
+    );
 }
